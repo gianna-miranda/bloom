4010569523278b2114341394a256ddd3bcc10dd0
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2019-11-14 09:11:01 -0500

    Finally

--- bloom/src/Components/NavBar/Navbar.jsx
+++ bloom/src/Components/NavBar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+// import Logo from './images/logo.png'
 import './navbar.scss'
 
 const Navbar = () =>{
     return(
         <nav>
             <header>
-                <h1>Bloom</h1>
+                {/* <img src={Logo} alt="Bloom Logo"/> */}
+                <h1>BLOOM</h1>
             </header>
             <div className="pages">
                 <a href="/"><p>Home</p></a>
                 <a href="/mentors"><p>Mentors</p></a>
                 <a href="/student"><p>Student</p></a>
-                <a href="/network"><p>Networking</p></a>
                 <a href="/about"><p>About</p></a>
             </div>
         </nav>
